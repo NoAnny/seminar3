49371843f47b14510d913809da01c8e728f782f7
NoAnnY <noanny@mail.ru>
2022-11-12 09:17:53 +0300

    thirdPower

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N:");
+int length = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Таблица кубов чисел от 1 до N: ");
+for (int i = 1; i < length+1; i++)
+{
+   Console.WriteLine($"{i}*{i}*{i} ={i * i * i}"); 
+}
